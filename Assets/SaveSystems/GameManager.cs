4d8380b458b66ed823021540faa78417cbac1a13
A1rborn3 <ethan.young.nz@gmail.com>
2025-09-14 16:57:50 +1200

    Save classes built
    fully modular save system allowing us to add as many variables as need and then converting them to Json

---
+++ Assets/SaveSystems/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public string currentSlot = "Save0"; //this is default. can be changed to support multiple files
+
+    public void SaveGame()
+    {
+        SaveData data = new SaveData(); //creates new data to hold all the new save data
+
+        foreach (var saveable in UnityEngine.Object.FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None))
+        {
+            if (saveable is ISaveable s) 
+                s.Save(data);
+        }
+
+        SaveManager.SaveGame(data, currentSlot);
+    }
+
+    public void LoadGame()
+    {
+        SaveData data = SaveManager.LoadGame(currentSlot);
+
+        foreach (var saveable in UnityEngine.Object.FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None))
+        {
+            if (saveable is ISaveable s)
+                s.Load(data);
+        }
+    }
+}
---
+++ Assets/SaveSystems/SaveData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class SaveData
+{
+    public int galaxySeed;
+    public List<string> destroyedPlanets = new List<string>();
+    public InventoryData inventory = new InventoryData();
+    public List<string> upgrades = new List<string>();
+
+
+
+
+
+}
+
+

8b053b1aefb565a1750022f7bbb2709e895fade5
A1rborn3 <ethan.young.nz@gmail.com>
2025-09-20 16:03:29 +1200

    Collisions added, astroid data added, shooting mechanic pending

---
+++ Assets/Scripts_Character/Laser.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float speed = 20f;// all upgradable
+    public int damage = 25;
+    public float lifetime = 2f; // destroy after 2s, can be changed with upgrades but ensures it doesnt exist forever
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = transform.right * speed; // shoot along local X axis
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) //asseses the collision of the laser
+    {
+        Astroid asteroid = collision.GetComponent<Astroid>();
+        if (asteroid != null)
+        {
+            asteroid.TakeDamage(damage);
+            Destroy(gameObject); // laser disappears on hit
+        }
+    }
+}
---
+++ Assets/Space Objects/Scripts/Astroid.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Astroid : MonoBehaviour
+{
+    public string asteroidName;   //set up with spawn control
+    public int maxHealth = 100;
+    private int currentHealth;
+    public Vector2 Pos;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
